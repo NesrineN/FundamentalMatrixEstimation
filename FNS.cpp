759d2b948d28373ac491ba9fad4591d431af81e0
NesrineN <nisrine.nohman@gmail.com>
2026-05-04 15:10:03 +0200

    Fixed FNS through Hartley Normalization though it is still giving a significant epipolar error even with no noise introduced

--- FNS.cpp
+++ FNS.cpp
@@ -145,64 +145,6 @@ Mat FNS(const Vec& u, const Mat& Eall, const std::vector<Mat>& Vall){
     return F;
 }
 
-// CFNS is when we force det(F)=0 during the iterations and not as a post-processing step
-// Mat CFNS(const Vec& u, const Mat& Eall){
-    
-//     Vec uold=u;
-//     Vec unew=u;
-
-//     for(int i=0; i<100; i++){
-//         Mat M=ComputeM(uold, Eall);
-//         Mat L=ComputeL(uold,Eall);
-
-//         Mat ML=M-L;
-
-//         Mat U(9,9);
-//         Mat V(9,9);
-//         Vec S(9);
-//         ML.SVD(U,S,V);
-
-//         int minIndex = 0;
-//         for (int i = 1; i < 9; ++i)
-//             if (S(i) < S(minIndex))
-//                 minIndex = i;
-
-//         unew = V.col(minIndex);
-
-//         if((unew-uold).qnorm()<1e-10){break;}
-
-//         uold=unew;
-
-//     }
-//     // the solution is unew
-//     Mat F=Mat::zeros(9);
-
-//     for(int i=0;i<3; i++){
-//         for(int j=0; j<3; j++){
-//             F(i, j) = unew(i * 3 + j);
-//         }
-//     }
-
-//     // enforcing rank 2 on F: 
-
-//     Mat U(3,3);
-//     Mat V(3,3);
-//     Vec S(3);
-//     F.SVD(U,S,V);
-
-//     int minIndex = 0;
-//         for (int i = 1; i < 3; ++i)
-//             if (S(i) < S(minIndex))
-//                 minIndex = i;
-
-//     S(minIndex)=0;
-
-//     Mat Sdiag = Mat::zeros(3,3);
-//     for(int i=0; i<3; i++) Sdiag(i,i) = S(i);
-
-//     F= U*Sdiag*V.t();
-
-//     return F;
-// }
+// CFNS
 
 
